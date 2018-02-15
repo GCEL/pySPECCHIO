5270aad45792c99b854408920e433478c042218e
Declan Valters <declan.valters@gmail.com>
2018-02-15 11:07:25 -0500

    Test script for specchio database connection

--- specchio_connect.py
+++ specchio_connect.py
@@ -1,7 +1,23 @@
 import jpype as jp
+import unittest
 
-jp.startJVM(jp.getDefaultJVMPath(), "-ea", "/usr/local/SPECCHIO/specchio-client.jar")
+jp.startJVM(jp.getDefaultJVMPath(), "-ea", "-Djava.class.path=/usr/local/SPECCHIO/specchio-client.jar")
 
+
+SPECCHIO = jp.JPackage('ch').specchio.client
+
+# Craete a client factor instance and get a list of all connection details
+cf = SPECCHIO.SPECCHIOClientFactory.getInstance()
+db_descriptor_list = cf.getAllServerDescriptors()
+
+# Createa a client for the first entry of the database connection list
+specchio_client = cf.createClient(db_descriptor_list.get(0))
+
+# No error return indicates succesful connection
+
+# See what server you are connected to
+db_descriptor = db_descriptor_list.get(0)
+db_descriptor.getDataSourceName()
 
 
 
