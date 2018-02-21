d76074644d2218ed30f3b4d2b6551c6ad71d75df
Declan Valters <declan.valters@gmail.com>
2018-02-21 11:30:34 -0500

    Init method for Java VM

--- specchio_connect.py
+++ specchio_connect.py
@@ -1,26 +1,33 @@
 import jpype as jp
 import unittest
 
-jp.startJVM(jp.getDefaultJVMPath(), "-ea", "-Djava.class.path=/usr/local/SPECCHIO/specchio-client.jar")
+def init_jvm(jvmpath=None):
+    if jp.isJVMStarted():
+        return
+    jp.startJVM(jp.getDefaultJVMPath(), "-ea", "-Djava.class.path=/usr/local/SPECCHIO/specchio-client.jar")
 
+init_jvm()
 
 SPECCHIO = jp.JPackage('ch').specchio.client
 
-# Craete a client factor instance and get a list of all connection details
+# Create a client factor instance and get a list of all connection details
 cf = SPECCHIO.SPECCHIOClientFactory.getInstance()
 db_descriptor_list = cf.getAllServerDescriptors()
 
-# Createa a client for the first entry of the database connection list
+# Create a client for the first entry of the database connection list
 specchio_client = cf.createClient(db_descriptor_list.get(0))
 
 # No error return indicates succesful connection
 
 # See what server you are connected to
 db_descriptor = db_descriptor_list.get(0)
-db_descriptor.getDataSourceName()
+print(db_descriptor.getDataSourceName())
 
 # Create a query object
 QUERIES = jp.JPackage('ch').specchio.queries
 query = QUERIES.Query()
 
+# Create a query conditio for the altitude attribute and configure it
+attr = specchio_client.getAttributesNameHash().get('Altitude')
 
+jp.shutdownJVM()
