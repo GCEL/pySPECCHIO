22cb13f79f99ab56f078be1f8fc739459d591352
Declan Valters <declan.valters@ed.ac.uk>
2018-02-23 15:19:50 +0000

    Custom java class python binding

---
+++ JPype_examples/Javabinding.py
@@ -0,0 +1,13 @@
+import jpype as jp
+
+jp.startJVM(jp.getDefaultJVMPath(), "-ea")
+
+testpackage = jp.JPackage('Test')
+Test = testpackage
+
+Test.speak("Hello from a custom Java class")
+t = Test()
+print t.getString()
+
+jp.shutdownJVM()
+
