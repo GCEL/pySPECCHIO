435d9fab2f4846648618c604a6001e1a04ceca57
Declan Valters <declan.valters@gmail.com>
2018-02-23 14:15:24 -0500

    More API experimentation

--- specchio_upload_test.py
+++ specchio_upload_test.py
@@ -1,4 +1,5 @@
 import jpype as jp
+import time
 import unittest
 
 def init_jvm(jvmpath=None):
@@ -13,16 +14,35 @@ init_jvm()
 
 spclient = jp.JPackage('ch').specchio.client
 spquery = jp.JPackage('ch').specchio.queries
+sptypes = jp.JPackage('ch').specchio.types
+spgui = jp.JPackage('ch').specchio.gui
+spreader_campaign = jp.JPackage('ch').specchio.file.reader.campaign
 
-print(spclient)
-print(spquery)
+# Connect to server
+client_factory = spclient.SPECCHIOClientFactory.getInstance()
+descriptor_list = client_factory.getAllServerDescriptors()
+specchio_client = client_factory.createClient(descriptor_list.get(0))
+
+# Set up a campaign data loader
+cdl = spreader_campaign.SpecchioCampaignDataLoader(specchio_client)
+
+campaign = specchio_client.getCampaign(18)
+
+cdl.set_campaign(campaign)
+cdl.start()
+
+delay = 0.01
+
+while cdl.isAlive:
+    time.sleep(delay)
+
+print('Number of parsed files: ', cdl.getParsed_file_Counter)
+print('Number of inserted files: ', cdl.getSuccessful_file_counter)    
+
 
 #class testSpecchio(unittest.TestCase):
 #
 #  def test_bad_package(self):
-spfoobarpkg = jp.JPackage('ch').specchio.foobar
 
-# todo: doesn't fail: probably needs to actually call a Java method from 
-# the class before failing??
 
 
