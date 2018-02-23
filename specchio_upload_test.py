39d9ff20182f46b321e6e4464ae0e9edec6388de
Declan Valters <declan.valters@gmail.com>
2018-02-23 11:55:54 -0500

    Unittest (fails)

--- specchio_upload_test.py
+++ specchio_upload_test.py
@@ -14,4 +14,15 @@ init_jvm()
 spclient = jp.JPackage('ch').specchio.client
 spquery = jp.JPackage('ch').specchio.queries
 
+print(spclient)
+print(spquery)
+
+#class testSpecchio(unittest.TestCase):
+#
+#  def test_bad_package(self):
+spfoobarpkg = jp.JPackage('ch').specchio.foobar
+
+# todo: doesn't fail: probably needs to actually call a Java method from 
+# the class before failing??
+
 
