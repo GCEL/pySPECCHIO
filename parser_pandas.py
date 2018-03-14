11f897f67669a9528018fe71b7a0589b4f4d6447
Declan Valters <declan.valters@ed.ac.uk>
2018-03-14 16:37:54 +0000

    dummy version of dataframe reader

--- parser_pandas.py
+++ parser_pandas.py
@@ -6,14 +6,24 @@ This is a temporary script file.
 """
 
 import os
+import pandas as pd
 
 datadir = "/home/dvalters/Projects/SPECCHIO/DATA/"
 
+dataframes = []
+csvs = []
+
+
+
 def tree_files():
     for (dirname, subdirs, files) in os.walk(datadir):
-        print('[' + dirname + ']')
+        #print('[' + dirname + ']')
         for fname in files:
-            print(os.path.join(dirname, fname))
+            filefullname = os.path.join(dirname, fname)
+            #print(filefullname)
+            dictname = os.path.splitext(os.path.basename(fname))[0]
+            print(dictname)
+            dataframes[dictname] = pd.readcsv(filefullname)
 
 if __name__=='__main__':
     tree_files()
