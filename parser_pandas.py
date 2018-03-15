dd36389139ff5fd4a4295290d270bfa9f8236b46
Declan Valters <declan.valters@ed.ac.uk>
2018-03-15 12:16:47 +0000

    Exclude the silly excel recovery files with regex

--- parser_pandas.py
+++ parser_pandas.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 
 import os
 import re
+import unittest
 import pandas as pd
 
 datadir = "/home/dvalters/Projects/SPECCHIO/DATA/"
@@ -20,7 +21,8 @@ def tree_files():
     for (dirname, subdirs, files) in os.walk(datadir):
         #print('[' + dirname + ']')
         for fname in files:
-            if re.match("^.*.xlsx$", fname):
+            # Only match "xlsx" files, exclude recovery/backup files
+            if re.match("^(?![~$]).*.xlsx$", fname):
                 filefullname = os.path.join(dirname, fname)
                 #print(filefullname)
                 dictname = os.path.splitext(os.path.basename(fname))[0]
@@ -29,3 +31,7 @@ def tree_files():
 
 if __name__=='__main__':
     tree_files()
+    
+    
+class parserTest(unittest.TestCase):
+    pass
