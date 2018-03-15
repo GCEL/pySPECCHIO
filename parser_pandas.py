1d0c52f64ecaccc610c551f5a856e7bdcc8d2cf1
Declan Valters <declan.valters@ed.ac.uk>
2018-03-15 12:08:41 +0000

    Fix regex to match xlsx, also using read_excel for now..

--- parser_pandas.py
+++ parser_pandas.py
@@ -6,11 +6,12 @@ This is a temporary script file.
 """
 
 import os
+import re
 import pandas as pd
 
 datadir = "/home/dvalters/Projects/SPECCHIO/DATA/"
 
-dataframes = []
+dataframes = {}
 csvs = []
 
 
@@ -19,11 +20,12 @@ def tree_files():
     for (dirname, subdirs, files) in os.walk(datadir):
         #print('[' + dirname + ']')
         for fname in files:
-            filefullname = os.path.join(dirname, fname)
-            #print(filefullname)
-            dictname = os.path.splitext(os.path.basename(fname))[0]
-            print(dictname)
-            dataframes[dictname] = pd.readcsv(filefullname)
+            if re.match("^.*.xlsx$", fname):
+                filefullname = os.path.join(dirname, fname)
+                #print(filefullname)
+                dictname = os.path.splitext(os.path.basename(fname))[0]
+                print(dictname)
+                dataframes[dictname] = pd.read_excel(filefullname)
 
 if __name__=='__main__':
     tree_files()
