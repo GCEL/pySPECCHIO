8569c8cde050e37da7fb05234e24ea815848f5bf
Declan Valters <declan.valters@gmail.com>
2018-04-05 10:23:44 -0400

    JSON normalize

--- spectra_parser.py
+++ spectra_parser.py
@@ -11,6 +11,7 @@ or pandas
 
 import json
 import pandas as pd
+from pandas.io.json import json_normalize
 
 path = "/home/centos/Downloads/DATA/Spectra_dir/"
 spectra_file_test = "QEP1USB1_b000000_s000002_light.pico"
@@ -19,7 +20,6 @@ def read_json():
     """Simple JSON reader"""
     with open(path + spectra_file_test, "r") as f:
         data = json.load(f)
-        print(type(data))
         return data
 
 def pandas_read_json():
@@ -29,7 +29,15 @@ def pandas_read_json():
     dataframe = pd.DataFrame(data)
     return dataframe
 
+def pandas_read_json_spectra():
+    """Normalise the results to get the spectra"""
+    with open(path + spectra_file_test, "r") as f:
+        data = json.load(f)
+        result = json_normalize(data["Spectra"])
+    return result    
+
 if __name__ == "__main__":
     data1 = read_json()
     df = pandas_read_json()
+    df2 = pandas_read_json_spectra()
     
